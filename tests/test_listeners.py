d37d4668adbbfde3edf5421c9bcd17dce810493b
burnscr <86748253+burnscr@users.noreply.github.com>
2024-08-25 00:54:26 -0400

    add tests for event listeners

--- tests/test_listeners.py
+++ tests/test_listeners.py
@@ -1,202 +1,195 @@
-from unittest import TestCase
+import pytest
 
+# noinspection PyProtectedMember
 from events.listeners import (
     _EventListener as EventListener,
-    EventListeners
+    EventListeners,
 )
 
 
-def callback_a():
-    pass
+def callback_a(): pass
+def callback_b(): pass
 
 
-def callback_b():
-    pass
+@pytest.fixture
+def default_priority():
+    return 10
 
 
-class TestEventListener(TestCase):
+@pytest.fixture
+def listeners(default_priority: int):
+    return EventListeners(default_priority)
+
+
+class TestEventListener:
 
     def test_equals_same_callback_same_priority(self):
         a = EventListener(callback_a, priority=0)
         b = EventListener(callback_a, priority=0)
-        self.assertEqual(a, b)
+        assert a == b
 
     def test_equals_same_callback_different_priority(self):
         a = EventListener(callback_a, priority=0)
         b = EventListener(callback_a, priority=1)
-        self.assertEqual(a, b)
+        assert a == b
 
     def test_equals_different_callback_same_priority(self):
         a = EventListener(callback_a, priority=0)
         b = EventListener(callback_b, priority=0)
-        self.assertNotEqual(a, b)
+        assert a != b
 
     def test_equals_different_callback_different_priority(self):
         a = EventListener(callback_a, priority=0)
         b = EventListener(callback_b, priority=1)
-        self.assertNotEqual(a, b)
+        assert a != b
 
-    def test_equals_unsupported_type(self):
+    def test_equals_wrong_type(self):
         a = EventListener(callback_a, priority=0)
         b = 'string'
-        self.assertNotEqual(a, b)
+        assert a != b
 
     def test_compare_same_callback_same_priority(self):
         a = EventListener(callback_a, priority=0)
         b = EventListener(callback_a, priority=0)
-        self.assertFalse(a < b, f'{a!r} less than {b!r}')
+        assert not a < b, f'{a!r} less than {b!r}'
 
     def test_compare_same_callback_higher_priority(self):
         a = EventListener(callback_a, priority=0)
         b = EventListener(callback_a, priority=1)
-        self.assertLess(a, b)
+        assert a < b
 
     def test_compare_same_callback_lower_priority(self):
         a = EventListener(callback_a, priority=1)
         b = EventListener(callback_a, priority=0)
-        self.assertFalse(a < b, f'{a!r} less than {b!r}')
+        assert not a < b, f'{a!r} less than {b!r}'
 
     def test_compare_different_callback_same_priority(self):
         a = EventListener(callback_a, priority=0)
         b = EventListener(callback_b, priority=0)
-        self.assertFalse(a < b, f'{a!r} less than {b!r}')
+        assert not a < b, f'{a!r} less than {b!r}'
 
     def test_compare_different_callback_higher_priority(self):
         a = EventListener(callback_a, priority=0)
         b = EventListener(callback_b, priority=1)
-        self.assertLess(a, b)
+        assert a < b
 
     def test_compare_different_callback_lower_priority(self):
         a = EventListener(callback_a, priority=1)
         b = EventListener(callback_b, priority=0)
-        self.assertFalse(a < b, f'{a!r} less than {b!r}')
+        assert not a < b, f'{a!r} less than {b!r}'
 
-    def test_compare_unsupported_type(self):
+    def test_compare_wrong_type(self):
         a = EventListener(callback_a, priority=0)
         b = 'string'
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             # noinspection PyTypeChecker
             a.__lt__(b)
 
 
-class TestEventListeners(TestCase):
+class TestEventListeners:
 
-    def setUp(self):
-        self.default_priority = default_priority = 10
-        self.listeners = EventListeners(default_priority)
+    def test_priority_default_type(self, listeners, default_priority):
+        priority = listeners.priority(None)
+        assert priority == default_priority
 
-    def test_priority_default_type(self):
-        priority = self.listeners.priority(None)
-        self.assertEqual(priority, self.default_priority)
+    @pytest.mark.parametrize('priority', [-20, -1, 0, 1, 10, 500])
+    def test_priority_int_value(self, listeners, priority):
+        new_priority = listeners.priority(priority)
+        assert priority == new_priority
 
-    def test_priority_int_value(self):
-        test_values = [-20, -1, 0, 1, 10, 500]
+    @pytest.mark.parametrize('priority', ['11', 25.8, callback_a])
+    def test_priority_wrong_type(self, listeners, priority):
+        with pytest.raises(TypeError):
+            # noinspection PyTypeChecker
+            listeners.priority(priority)
 
-        for old_priority in test_values:
-            with self.subTest(old_priority):
+    def test_get_event_callbacks(self, listeners, default_priority):
+        listeners.add_event_callback('event', callback_a, default_priority)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_a,),)
 
-                new_priority = self.listeners.priority(old_priority)
-                self.assertEqual(old_priority, new_priority)
+    def test_get_event_callbacks_empty(self, listeners):
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ()
 
-    def test_priority_unsupported_type(self):
-        test_values = ['11', 25.8, callback_a]
+    def test_get_event_callbacks_same_priority(self, listeners):
+        listeners.add_event_callback('event', callback_a, priority=1)
+        listeners.add_event_callback('event', callback_b, priority=1)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_a, callback_b),)
 
-        for priority in test_values:
-            with self.subTest(priority):
+    def test_get_event_callbacks_diff_priority(self, listeners):
+        listeners.add_event_callback('event', callback_a, priority=2)
+        listeners.add_event_callback('event', callback_b, priority=1)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_b,), (callback_a,))
 
-                with self.assertRaises(TypeError):
-                    # noinspection PyTypeChecker
-                    self.listeners.priority(priority)
+    def test_get_event_callbacks_duplicate(self, listeners, default_priority):
+        listeners.add_event_callback('event', callback_a, default_priority)
+        listeners.add_event_callback('event', callback_a, default_priority)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_a,),)
 
-    def test_get_event_callbacks_empty(self):
-        callbacks = self.listeners.get_event_callbacks('missing')
+    def test_get_event_callbacks_multi_events(self, listeners, default_priority):
+        listeners.add_event_callback('event', callback_a, default_priority)
+        listeners.add_event_callback('other', callback_a, default_priority)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_a,),)
+
+    def test_get_event_callbacks_diff_event(self, listeners, default_priority):
+        listeners.add_event_callback('other', callback_a, default_priority)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ()
+
+    def test_get_event_callbacks_removed(self, listeners, default_priority):
+        listeners.add_event_callback('event', callback_a, default_priority)
+        listeners.remove_event_callback('event', callback_a)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ()
+
+    def test_get_event_callbacks_wrong_type(self, listeners):
         # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ())
+        callbacks = listeners.get_event_callbacks(15)
+        assert callbacks == ()
 
-    def test_get_event_callbacks_single_item(self):
-        self.listeners.add_event_callback('single', callback_a, 1)
-        callbacks = self.listeners.get_event_callbacks('single')
+    def test_add_event_callback(self, listeners, default_priority):
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ()
+
+        listeners.add_event_callback('event', callback_a, default_priority)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_a,),)
+
+    def test_add_event_callback_new_priority(self, listeners):
+        listeners.add_event_callback('event', callback_a, priority=1)
+        listeners.add_event_callback('event', callback_b, priority=2)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_a,), (callback_b,))
+
+        listeners.add_event_callback('event', callback_a, priority=2)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_b, callback_a),)
+
+        listeners.add_event_callback('event', callback_b, priority=1)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_b,), (callback_a,))
+
+    def test_remove_event_callback(self, listeners, default_priority):
+        listeners.add_event_callback('event', callback_a, default_priority)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ((callback_a,),)
+
+        listeners.remove_event_callback('event', callback_a)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ()
+
+    def test_remove_event_callback_missing(self, listeners):
+        listeners.remove_event_callback('event', callback_a)
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ()
+
+    def test_remove_event_callback_wrong_type(self, listeners):
         # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ((callback_a,),))
-
-    def test_get_event_callbacks_multiple_items_same_priority(self):
-        self.listeners.add_event_callback('multiple', callback_a, 1)
-        self.listeners.add_event_callback('multiple', callback_b, 1)
-        callbacks = self.listeners.get_event_callbacks('multiple')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ((callback_a, callback_b),))
-
-    def test_get_event_callbacks_multiple_items_different_priority(self):
-        self.listeners.add_event_callback('multiple', callback_a, 2)
-        self.listeners.add_event_callback('multiple', callback_b, 1)
-        callbacks = self.listeners.get_event_callbacks('multiple')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ((callback_b,), (callback_a,)))
-
-    def test_get_event_callbacks_single_item_duplicate_entries(self):
-        self.listeners.add_event_callback('single', callback_a, 1)
-        self.listeners.add_event_callback('single', callback_a, 1)
-        callbacks = self.listeners.get_event_callbacks('single')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ((callback_a,),))
-
-    def test_get_event_callbacks_single_item_multiple_events(self):
-        self.listeners.add_event_callback('single', callback_a, 1)
-        self.listeners.add_event_callback('other', callback_a, 1)
-        callbacks = self.listeners.get_event_callbacks('single')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ((callback_a,),))
-
-    def test_get_event_callbacks_empty_item_different_event(self):
-        self.listeners.add_event_callback('other', callback_a, 1)
-        callbacks = self.listeners.get_event_callbacks('missing')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ())
-
-    def test_get_event_callbacks_emtpy_item_removed_entry(self):
-        self.listeners.add_event_callback('event', callback_a, 1)
-        self.listeners.remove_event_callback('event', callback_a)
-        callbacks = self.listeners.get_event_callbacks('event')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ())
-
-    def test_get_event_callbacks_unexpected_type(self):
-        # noinspection PyTypeChecker
-        callbacks = self.listeners.get_event_callbacks(15)
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ())
-
-    def test_add_event_callback_same_callback_different_priority(self):
-        self.listeners.add_event_callback('event', callback_a, 1)
-        self.listeners.add_event_callback('event', callback_b, 1)
-
-        callbacks = self.listeners.get_event_callbacks('event')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ((callback_a, callback_b),))
-
-        self.listeners.add_event_callback('event', callback_a, 2)
-
-        callbacks = self.listeners.get_event_callbacks('event')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ((callback_b,), (callback_a,)))
-
-    def test_remove_event_callback(self):
-        self.listeners.add_event_callback('event', callback_a, 1)
-
-        callbacks = self.listeners.get_event_callbacks('event')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ((callback_a,),))
-
-        self.listeners.remove_event_callback('event', callback_a)
-
-        callbacks = self.listeners.get_event_callbacks('event')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ())
-
-    def test_remove_event_callback_missing(self):
-        # (no exception raised)
-        self.listeners.remove_event_callback('event', callback_a)
-
-        callbacks = self.listeners.get_event_callbacks('event')
-        # noinspection PyTypeChecker
-        self.assertTupleEqual(callbacks, ())
+        listeners.remove_event_callback('event', 'callback')
+        callbacks = listeners.get_event_callbacks('event')
+        assert callbacks == ()
